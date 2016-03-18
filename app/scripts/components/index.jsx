e5e46d04347afafd413978416fa3fdd2eefd6949
Hannah Smith <hannahsmith@Hannahs-MacBook-Pro.local>
2016-03-18 12:03:34 -0400

    rendered hours,contact,menu with react to screen

---
+++ app/scripts/components/index.jsx
@@ -0,0 +1,53 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jquery');
+
+var HoursComponent = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <a href="#">Hours:</a>
+          <h2>Lunch:</h2>
+            <p>Mon-Sat: 11:00am-2:00pm</p>
+          <h2>Dinner:</h2>
+            <p>Mon-Sat: 5:00pm-10:30pm</p>
+      </div>
+    );
+  }
+})
+
+var ContactComponent = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h1>Contact</h1>
+          <h3>864-555-5555</h3>
+      </div>
+    );
+  }
+})
+
+var MenuComponent = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <a href="#">Menu</a>
+          <ul>
+            <li>Appetizers</li>
+            <li>Salads</li>
+            <li>Lunch</li>
+            <li>Dinner</li>
+            <li>Drinks</li>
+          </ul>
+      </div>
+    )
+  }
+})
+
+
+
+module.exports = {
+  'HoursComponent': HoursComponent,
+  'ContactComponent': ContactComponent,
+  'MenuComponent': MenuComponent
+}
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jquery');
+
+var models = require('./models/models');
+var restaurantCollection = new models.RestaurantCollection();
+
+var restaurantComponent = require('./components/index.jsx');
+var HoursComponent = restaurantComponent.HoursComponent;
+var ContactComponent = restaurantComponent.ContactComponent;
+var MenuComponent = restaurantComponent.MenuComponent;
+
+ReactDOM.render(
+  React.createElement(HoursComponent),
+  document.getElementById('app')
+);
+
+ReactDOM.render(
+  React.createElement(ContactComponent),
+  document.getElementById('contact')
+);
+
+ReactDOM.render(
+  React.createElement(MenuComponent),
+  document.getElementById('menu')
+);
+
+restaurantCollection.fetch();
